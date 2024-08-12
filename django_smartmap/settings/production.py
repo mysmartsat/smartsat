8d9883f8a1707b1dbaa567fe3f26cfb920ea9c44
Hongyu Wang <walkon107@gmail.com>
2024-08-12 15:19:35 -0500

    Demo finished. Roll back to original version.

--- django_smartmap/settings/production.py
+++ django_smartmap/settings/production.py
@@ -18,7 +18,7 @@ if os.getenv("GOOGLE_CLOUD_PROJECT", None):
     project_id = os.getenv("GOOGLE_CLOUD_PROJECT")
 
     client = secretmanager.SecretManagerServiceClient()
-    settings_name = os.getenv("SETTINGS_NAME", "django-demo")
+    settings_name = os.getenv("SETTINGS_NAME", "application_settings")
     name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
     payload = client.access_secret_version(name=name).payload.data.decode(
         "UTF-8"
